ea21775828aa762290509394f8d01349ad2978e2
Josh Fermin <joshfermin@gmail.com>
2019-07-30 16:58:18 -0600

    split into gradle subprojects and finish poc from client side

--- hello-world-client/src/main/kotlin/helloworld/HelloWorldClient.kt
+++ hello-world-client/src/main/kotlin/helloworld/HelloWorldClient.kt
@@ -1,27 +1,91 @@
 package helloworld
 
-import com.google.common.annotations.VisibleForTesting
+import io.grpc.ClientInterceptor
 import java.util.Random
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.CountDownLatch
-import java.io.IOException
 import io.grpc.stub.StreamObserver
 import io.grpc.StatusRuntimeException
 import io.grpc.ManagedChannelBuilder
 import io.grpc.ManagedChannel
 import io.grpc.Status
 import mu.KotlinLogging
-import javax.inject.Singleton
+import io.grpc.ForwardingClientCall
+import io.grpc.ClientCall
+import io.grpc.CallOptions
+import io.grpc.Channel
+import io.grpc.ClientInterceptors
+import io.grpc.Metadata
+import io.grpc.MethodDescriptor
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.Option
+import org.apache.commons.cli.Options
+import org.apache.commons.cli.ParseException
 
-@Singleton
-class GreetingClient(name: String, port: Int) {
+
+class HelloWorldClient(name: String, apiKey: String) {
     companion object {
         private val logger = KotlinLogging.logger {}
+
+        /** Issues several different requests and then exits.  */
+        @Throws(InterruptedException::class)
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val options = createOptions()
+            val parser = DefaultParser()
+            val params: CommandLine
+            try {
+                params = parser.parse(options, args)
+            } catch (e: ParseException) {
+                System.err.println("Invalid command line: $e")
+                return
+            }
+
+            val address = params.getOptionValue("host")
+            val apiKey = params.getOptionValue("api_key")
+
+            val client = HelloWorldClient(address, apiKey)
+            try {
+                client.sayHello("Test")
+                client.lotsOfReplies("Test")
+                client.lotsOfGreetings(listOf("Test", "Other Test", "Second Test"))
+                val finishLatch = client.bidiHello()
+                if (!finishLatch.await(1, TimeUnit.MINUTES)) {
+                    logger.warn("bidirectional test did not finish within 1 minutes")
+                }
+            } finally {
+                client.shutdown()
+            }
+        }
+
+        private fun createOptions(): Options {
+            val options = Options()
+
+            options.addOption(Option.builder()
+                    .longOpt("host")
+                    .desc("The address of the gRPC server")
+                    .hasArg()
+                    .argName("host")
+                    .type(String::class.java)
+                    .build())
+
+            options.addOption(Option.builder()
+                    .longOpt("api_key")
+                    .desc("The API key to use for RPC calls")
+                    .hasArg()
+                    .argName("key")
+                    .type(String::class.java)
+                    .build())
+
+            return options
+        }
     }
 
-    private val channel: ManagedChannel = ManagedChannelBuilder.forAddress(name, port).build()
-    private val blockingStub: GreeterGrpc.GreeterBlockingStub = GreeterGrpc.newBlockingStub(channel)
-    private var asyncStub: GreeterGrpc.GreeterStub = GreeterGrpc.newStub(channel)
+    private val channel: ManagedChannel = ManagedChannelBuilder.forTarget(name).usePlaintext().build()
+    private val ch: Channel = ClientInterceptors.intercept(channel, Interceptor(apiKey))
+    private val blockingStub: GreeterGrpc.GreeterBlockingStub = GreeterGrpc.newBlockingStub(ch)
+    private var asyncStub: GreeterGrpc.GreeterStub = GreeterGrpc.newStub(ch)
 
     private var random = Random()
 
@@ -113,7 +177,7 @@ class GreetingClient(name: String, port: Int) {
 
         // Receiving happens asynchronously
         if (!finishLatch.await(1, TimeUnit.MINUTES)) {
-            logger.warn("client streaming can not finish within 1 minutes")
+            logger.warn("client streaming did not finish within 1 minute")
         }
     }
 
@@ -159,36 +223,25 @@ class GreetingClient(name: String, port: Int) {
         return finishLatch
     }
 
-    /** Issues several different requests and then exits.  */
-    @Throws(InterruptedException::class)
-    fun main(args: Array<String>) {
-        val client = GreetingClient("34.66.126.97", 80)
-        try {
-            // Looking for a valid feature
-            client.sayHello("Rener")
-
-            // Feature missing.
-            client.lotsOfReplies("Joe")
-
-            // Looking for features between 40, -75 and 42, -73.
-            client.lotsOfGreetings(listOf("Test", "Other Test", "Second Test"))
-
-            // Send and receive some notes.
-            val finishLatch = client.bidiHello()
-
-            if (!finishLatch.await(1, TimeUnit.MINUTES)) {
-                logger.warn("routeChat can not finish within 1 minutes")
+    private class Interceptor(private val apiKey: String?) : ClientInterceptor {
+        override fun <ReqT, RespT> interceptCall(
+                method: MethodDescriptor<ReqT, RespT>, callOptions: CallOptions, next: Channel): ClientCall<ReqT, RespT> {
+            LOGGER.info("Intercepted " + method.fullMethodName)
+            var call = next.newCall(method, callOptions)
+            call = object : ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(call) {
+                override fun start(responseListener: ClientCall.Listener<RespT>, headers: Metadata) {
+                    if (apiKey != null && !apiKey.isEmpty()) {
+                        LOGGER.info("Attaching API Key: $apiKey")
+                        headers.put(API_KEY_HEADER, apiKey)
+                    }
+                    super.start(responseListener, headers)
+                }
             }
-        } finally {
-            client.shutdown()
+            return call
         }
-    }
-
-    /**
-     * Only used for unit test, as we do not want to introduce randomness in unit test.
-     */
-    @VisibleForTesting
-    fun setRandom(random: Random) {
-        this.random = random
+        companion object {
+            private val LOGGER = KotlinLogging.logger {}
+            private val API_KEY_HEADER = Metadata.Key.of("x-api-key", Metadata.ASCII_STRING_MARSHALLER)
+        }
     }
 }
